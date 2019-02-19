8b3e699b4775d982c4ebe44f91e289ed74b82677
glennlgan <274671731qq.com>
2019-02-19 19:29:57 +0800

    组合模式

---
+++ Composite Pattern/src/Employee.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        String s =  ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]\n");
+        for(Employee employee : subordinates){
+            s = s + employee.toString();
+        }
+        return s;
+    }
+}
